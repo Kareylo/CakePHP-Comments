df5f5e53eb4b0f26cd0b57d39fe30cd664b9454c
Corentin Nazé <kareylo@live.be>
2017-03-28 08:38:41 +0000

    fix TestCase for Postgres

--- tests/TestCase/Model/Behavior/CommentableBehaviorTest.php
+++ tests/TestCase/Model/Behavior/CommentableBehaviorTest.php
@@ -1,18 +1,18 @@
 <?php
 namespace Kareylo\Comments\Test\TestCase\Model\Behavior;
 
+use App\Model\Table\PostsTable;
+use App\Model\Table\UsersTable;
+use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 
 class CommentableBehaviorTest extends TestCase
 {
     /**
-     * The instance of the model
-     *
-     * @var null
+     * @var PostsTable|null
      */
     public $Posts = null;
-    public $Users = null;
 
     public $fixtures = [
         'plugin.kareylo/comments.comments',
@@ -24,14 +24,12 @@ class CommentableBehaviorTest extends TestCase
     {
         parent::setUp();
         $this->Posts = TableRegistry::get('Posts');
-        $this->Users = TableRegistry::get('Users');
     }
 
     public function tearDown()
     {
         parent::tearDown();
         unset($this->Posts);
-        unset($this->Users);
         TableRegistry::clear();
     }
 
@@ -41,23 +39,23 @@ class CommentableBehaviorTest extends TestCase
     public function testFindCommentsWithoutComments()
     {
         $this->Posts->addBehavior('Kareylo/Comments.Commentable', []);
-        $result = $this->Posts->find()->where('Posts.id = 1')->find('comments')->first();
-        $expected = $this->Posts->find('all', ['Posts.id' => 1])->contain('Comments')->first();
+        $result = $this->Posts->find()->where(['id' => 1])->find('comments')->first();
+        $expected = $this->Posts->find()->where(['id' => 1])->contain('Comments')->first();
         $this->assertEquals($expected, $result);
     }
 
     public function testFindCommentsWithEmptyModelData()
     {
         $this->Posts->addBehavior('Kareylo/Comments.Commentable', []);
-        $result = $this->Posts->find()->where('Posts.id = 999')->find('comments')->first();
+        $result = $this->Posts->find()->where(['id' => 999])->find('comments')->first();
         $this->assertEquals(null, $result);
     }
 
     public function testFindCommentsWithModelData()
     {
         $this->Posts->addBehavior('Kareylo/Comments.Commentable', []);
-        $result = $result = $this->Posts->find()->where('Posts.id = 2')->find('comments')->first();
-        $expected = $result = $this->Posts->find()->where('Posts.id = 2')->contain(['Comments' => function ($q) {
+        $result = $result = $this->Posts->find()->where(['id' => 2])->find('comments')->first();
+        $expected = $this->Posts->find()->where(['id' => 2])->contain(['Comments' => function (Query $q) {
             return $q->find('threaded')->contain('Users');
         }])->first();
         $this->assertEquals($expected, $result);
